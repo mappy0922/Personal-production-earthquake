094f89997a533fcaf60650a4f4a747cd1f4a5b63
LAPTOP-SSDSJOVC\masah <mr.masa0329@gmail.com>
2026-06-05 18:29:06 +0900

    画面外処理実装途中

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,54 @@
 import * as d3 from "d3";
-import { useState , useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
+
+const MapName = [
+  "日本地図",
+  "世界地図",
+]
 
 export default function App() {
   const width = window.innerWidth;
   const height = window.innerHeight;
 
-  const MapName = [
-    {
-      name: "日本地図",
-    },
-    {
-      name: "世界地図",
-      position: {X: [0, width, -width], Y: [-13, height-13, -height-13]
-      },
-    },
-  ]
-
   const [Scale , setScale] = useState(1);
-  const [Map , setMap] = useState(MapName[0].name);
+  const [Map , setMap] = useState(MapName[0]);
 
-  const MapId = Map === "日本地図" ? "/日本地図.jpeg" : "/世界地図.png"
+  const MapId = {
+    "日本地図" : {
+      images: [
+        {
+          href: "/日本地図.jpeg",
+          x: 0,
+          y: 0,
+        },
+      ],
+    },
+
+    "世界地図" : {
+      images: [
+        {href: "map_1.png", x: -2*width, y: -height},
+        {href: "map_2.png", x: -width, y: -height},
+        {href: "map_3.png", x: 0, y: -height},
+        {href: "map_4.png", x: width, y: -height},
+        {href: "map_5.png", x: 2*width, y: -height},
+        {href: "map_6.png", x: 3*width, y: -height},
+
+        {href: "map_7.png", x: -2*width, y: 0},
+        {href: "map_8.png", x: -width, y: 0},
+        {href: "map_9.png", x: 0, y: 0},
+        {href: "map_10.png", x: width, y: 0},
+        {href: "map_11.png", x: 2*width, y: 0},
+        {href: "map_12.png", x: 3*width, y: 0},
+
+        {href: "map_13.png", x: -2*width, y: height},
+        {href: "map_14.png", x: -width, y: height},
+        {href: "map_15.png", x: 0, y: height},
+        {href: "map_16.png", x: width, y: height},
+        {href: "map_17.png", x: 2*width, y: height},
+        {href: "map_18.png", x: 3*width, y: height},
+      ],
+    },
+  }
 
   const svgRef = useRef();
   const zoomRef = useRef();
@@ -31,11 +60,37 @@ export default function App() {
     const imageLayer = svg.select("#imageLayer");
 
     const zoom = d3.zoom()
-    .scaleExtent([1, 14])
+    .scaleExtent([0.5, 14])
     .on("zoom", (event) => {
+      let x = event.transform.x;
+      let y = event.transform.y;
+      const k = event.transform.k;
+
+      if(Map === "日本地図") {
+        while (x > width) {
+          x-=2*width;
+        }
+
+        while (x < -width) {
+          x+=2*width;
+        }
+
+        while (y > height) {
+          y-=2*height;
+        }
+
+        while (y < -height) {
+          y+=2*height;
+        }
+      } /*else {
+        while(x > width) {
+          x-=6*width;
+        }
+      }*/
+
       imageLayer.attr(
         "transform",
-        event.transform
+        `translate(${x},${y}) scale(${k})`
       );
       setScale(Number(event.transform.k.toFixed(1)));
     });
@@ -45,7 +100,7 @@ export default function App() {
 
     svg.call(zoom);
 
-  }, []);
+  }, [Map]);
 
   const zoomIn = () => {
     const svg = d3.select(svgRef.current);
@@ -82,18 +137,16 @@ export default function App() {
 
         <div className="Map-Button">
           <button 
-          className="Reset"
           onClick={() => {
-            setMap(MapName[0].name);
+            setMap(MapName[0]);
             Reset();
           }}
           >日本地図
           </button>
 
           <button 
-          className="Reset"
           onClick={() => {
-            setMap(MapName[1].name);
+            setMap(MapName[1]);
             Reset();
           }}
           >世界地図
@@ -103,28 +156,19 @@ export default function App() {
         <svg ref={svgRef} width={width} height={height} >
           <g id="imageLayer">
             <>
-            <image
-            className="mapName"
-            href={MapId}
-            x="0"
-            y="-13"
-            width={width}
-            height={height}
-            />
-            {Map === "世界地図" && 
-            MapName[1].position.Y.map((y) => 
-              MapName[1].position.X.map((x) => (
+            {MapId[Map].images.map((m,i) => (
               <image
-              key={`${x}-${y}`}
-              href={MapId}
-              x={x}
-              y={y}
+              className="Japan"
+              key={i}
+              href={m.href}
+              x={m.x}
+              y={m.y}
               width={width}
               height={height}
               preserveAspectRatio="none"
               />
-            )))}
-
+            ))}
+        
             <line
             className="Number-of-people-moving"
             x1="500"
